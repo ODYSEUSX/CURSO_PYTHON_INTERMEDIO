fc63b9b4eb3e63ce95fcdc31e2884de2438099ca
ULISES <nexu_2001@hotmail.com>
2021-07-03 12:15:16 -0500

    tarea 2: usamos lis comprhension , high functions , con python3.8 y 3.8 , utilizamos las funciones en un proyecto

---
+++ filter.py
@@ -0,0 +1,11 @@
+from functools import reduce
+def run():
+
+    #Filter
+    myList = [1,4,5,7,9,13,19,21]
+
+    odd = list(filter(lambda x: x % 2 != 0, myList))
+    print(odd)
+
+if __name__=='__main__':
+    run()
