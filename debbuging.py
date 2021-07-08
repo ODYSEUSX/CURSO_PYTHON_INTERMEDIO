28962206eca24fd0ee2a94810312ab16b7308ef8
ULISES <nexu_2001@hotmail.com>
2021-07-08 12:12:34 -0500

    combinacion de try, raise, except

--- debbuging.py
+++ debbuging.py
@@ -1,11 +1,15 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
-
-
+    try:
+        if num<0 :
+            raise ValueError("Ingresa un numero positivo entero")
+        divisors = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
 
 def run():
     try:
